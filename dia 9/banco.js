5a9b4797051d36fd22f47062e0a581da58c8fdcb
Felipe Bezao <febezao@gmail.com>
2024-09-25 23:03:56 -0300

    CORRIGINDO O EXERCICIO

--- dia 9/banco.js
+++ dia 9/banco.js
@@ -1,15 +1,15 @@
-let continuar  
-do{
+let continuar 
 let nome = prompt("Digite seu nome")
 let idade = prompt("Digite sua idade")
-let salario = parseFloat(prompt("Digite seu salario"))
-
+let salario = parseFloat(prompt("Digite seu salario")) 
+do{
 console.log("Olá " + nome + ", você tem " + idade + " anos, e tem o salário de R$" + salario)
 continuar = prompt("As informações mostradas estão corretas? (s/n)")
 
 } while(continuar != "s")
     console.log("prosseguindo")
 
+//PREVISAO DO SALARIO
 let aumentoSalarial = 0.015
 console.log("Previsão salarial para os próximos 10 anos: ")
 
@@ -17,5 +17,5 @@ for(let ano = 1; ano <= 10; ano++){
     salario += salario * aumentoSalarial
     aumentoSalarial *= 2
 
-    console.log((2023 - ano)) + " = R$ " + salario
+    console.log((2023 + ano) + " = R$ " + salario)
 }
