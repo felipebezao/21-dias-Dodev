b6649f6f8bb059765b33af195f71c6e68b9ca306
Felipe Bezão <febezao@gmail.com>
2024-10-18 17:22:23 -0300

    att dia 13

--- dia 13/ex2.js
+++ dia 13/ex2.js
@@ -9,6 +9,41 @@ class Carro {
             this.VelocidadeMaxima = velocidadeMaxima
             this.Aceleracao = aceleracao
         }
+       CalcularTempoMedio(distancia){
+        let resultado = distancia / (this.VelocidadeMaxima / this.Aceleracao)
+        return resultado
+       }
 }
+class Corrida {
+    Nome
+    Tipo
+    Distancia
+    Participantes
+    Vencedor
 
-let carro = new Carro()
+        constructor(nome, tipo, distancia){
+            this.Nome = nome
+            this.Tipo = tipo
+            this.Distancia = distancia
+            this.Participantes = []
+            this.Vencedor = ""
+        }
+        DefinirVencedor(){
+            let menorTempo = this.Participantes[0].CalcularTempoMedio(this.Distancia)
+            let vencedor = this.Participantes[0]
+            
+            for(let index = 1; index <this.Participantes.length; index++) {
+                let tempo = this.Participantes[index].CalcularTempoMedio(this.Distancia)
+                if(tempo < menorTempo){
+                    menorTempo = tempo
+                    vencedor = this.Participantes[index]
+                }
+            }
+            return this.vencedor = vencedor
+        }
+            
+        ExibirVencedor(){
+            alert("O vencedor é: " + this.Vencedor.Nome)
+        }
+        
+    }
